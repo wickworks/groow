baaedae5bf72693a55a3e6d25f35910d3a0a8a58
Olive Perry <wickbox@gmail.com>
2022-08-07 14:26:31 +0100

    more plantlike starting nodes

--- Scenes/_Main.gd
+++ Scenes/_Main.gd
@@ -4,9 +4,11 @@ var BezierPointScene = preload("res://Scenes/BezierPoint/BezierPoint.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	AddPoint(Vector2(-80,160), Vector2(0,100))
-	AddPoint(Vector2(80,0), Vector2(-100,-40))
-	AddPoint(Vector2(0,-160), Vector2(20,-20))
+	AddPoint(Vector2(0,160), Vector2(0,100))
+	AddPoint(Vector2(100,80), Vector2(-30,80))
+	AddPoint(Vector2(-100,0), Vector2(30,80))
+	AddPoint(Vector2(100,-80), Vector2(30,80))
+	AddPoint(Vector2(0,-160), Vector2(0,100))
 
 func _unhandled_input(event):
 	if (event is InputEventMouseButton && event.pressed && event.button_index == BUTTON_LEFT):
