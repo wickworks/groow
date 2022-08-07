a5e156a7236fb0b717cb481a81eedddf23e2083e
Olive Perry <wickbox@gmail.com>
2022-08-07 13:01:24 +0100

    Handles are mirrored

--- Scenes/BezierPoint/BezierPoint.gd
+++ Scenes/BezierPoint/BezierPoint.gd
@@ -9,21 +9,27 @@ signal updatedPointOrHandle
 
 var draggingPoint = false
 var draggingHandle = false
+var draggingHandleMirror = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if $Handle.position == Vector2.ZERO:
-		$Handle.position = startingHandle
+		setHandlePosition(startingHandle)
+		
 	updateLine()	
 
 func setPositionAndHandle(selfPos, handlePos):
 	set_position(selfPos)
+	setHandlePosition(handlePos)
+
+func setHandlePosition(handlePos):
 	$Handle.set_position(handlePos)
-	updateLine()	
+	$HandleMirror.set_position(-handlePos)
+	updateLine()
 
 func updateLine():
 	$HandleLine.points = Array()
-	$HandleLine.add_point(Vector2.ZERO)
+	$HandleLine.add_point($HandleMirror.position)
 	$HandleLine.add_point($Handle.position)
 	
 	emit_signal("updatedPointOrHandle", indexInCurve)
@@ -37,8 +43,9 @@ func _process(delta):
 	var mousePos = get_global_mouse_position()
 	
 	if draggingHandle:
-		$Handle.position = mousePos - position
-		updateLine()
+		setHandlePosition(mousePos - position)
+	elif draggingHandleMirror:
+		setHandlePosition(position - mousePos)
 	elif draggingPoint:
 		position = mousePos
 		updateLine()
@@ -47,9 +54,16 @@ func _input(event):
 	if (event is InputEventMouseButton && !event.pressed):
 		draggingPoint = false
 		draggingHandle = false
+		draggingHandleMirror = false
 
 func _on_HandleButton_button_down():
 	draggingHandle = true
 
+func _on_MirrorButton_button_down():
+	draggingHandleMirror = true
+
 func _on_PointButton_button_down():
 	draggingPoint = true
+
+
+
